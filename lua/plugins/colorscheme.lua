e42f7383b421367e9428198a3dd3784fb93bce2b
Nazar Khudan <ankhudan10@gmail.com>
2024-09-11 12:17:40 +0300

    🐑 add files

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,57 @@
+function cmp()
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+cmp()
+
+return {
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = function()
+				require("catppuccin").load()
+			end,
+		},
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		opts = {
+			no_italic = true,
+			term_colors = true,
+			transparent_background = true,
+			styles = {
+				comments = {},
+				conditionals = {},
+				loops = {},
+				functions = {},
+				keywords = {},
+				strings = {},
+				variables = {},
+				numbers = {},
+				booleans = {},
+				properties = {},
+				types = {},
+			},
+			color_overrides = {
+				mocha = {
+					base = "#000000",
+					mantle = "#000000",
+					crust = "#000000",
+				},
+			},
+			integrations = {
+				telescope = {
+					enabled = true,
+					style = "nvchad",
+				},
+				dropbar = {
+					enabled = true,
+					color_mode = true,
+				},
+			},
+		},
+	},
+}
